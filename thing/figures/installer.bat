bc58bd590bfd67e970da01ebd448a488fdf64ab6
Boalbads <68003904+Boalbads@users.noreply.github.com>
2022-09-17 13:38:19 +0100

    Create installer.bat

---
+++ thing/figures/installer.bat
@@ -0,0 +1,17 @@
+echo off
+cls
+echo Make sure that the figure you want to install is,
+echo 1. Named character.rbxm
+echo 2. In the same folder that this script is in
+pause >nul
+cls
+echo 06blox should be installed via the installer, into the correct location,
+pause >nul
+cls
+copy character.rbxm "%windir%\..\Program Files\06blox\content\fonts"
+cls
+echo Your character should be replaced, if you join a game, it should have changed.
+echo Thank you for using this tool.
+pause >nul
+cls
+exit
